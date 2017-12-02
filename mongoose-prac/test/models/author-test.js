1b26000343b5cf12c3bf051cc4beceedccbdb4cb
Nick Duckwiler <nduckwiler@gmail.com>
2017-12-02 16:31:33 -0500

    test .create

--- mongoose-prac/test/models/author-test.js
+++ mongoose-prac/test/models/author-test.js
@@ -26,6 +26,38 @@ describe('Author', () => {
 
       assert.strictEqual(author.first_name, firstNameAsInt.toString());
     });
+  });
 
+  describe('.create', () => {
+    describe('when passed a doc', () => {
+      it('persists an author', async () => {
+        const fields = {
+					first_name: 'bob',
+					family_name: 'bobby',
+					phone: '555-555-5555'
+        };
+        const author = new Author(fields);
+
+        await Author.create(author);
+
+        const stored = await Author.findOne({ 'first_name': 'bob' });
+        assert.include(stored, fields);
+      });
+    });
+
+    describe('when passed properties', () => {
+      it('persists a author', async () => {
+        const fields = {
+					first_name: 'bob',
+					family_name: 'bobby',
+					phone: '555-555-5555'
+        };
+
+        await Author.create(fields);
+
+        const stored = await Author.findOne({ 'first_name': 'bob' });
+        assert.include(stored, fields);
+      });
+    });
   });
 });
