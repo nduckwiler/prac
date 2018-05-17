7843f2d20fd21ee3c974cecfe08ce13c3a232658
Nick Duckwiler <nduckwiler@gmail.com>
2018-05-17 12:55:26 -0400

    Clean up and comment

--- rewire-prac/test/index-test.js
+++ rewire-prac/test/index-test.js
@@ -1,33 +1,17 @@
 const rewire = require("rewire");
 const assert = require('chai').assert;
+
+// A mocked request.json
 const event = require('../request.json');
 
 // A mocked node module called 'alexa-sdk' is created in the node_modules folder
 var myModule = rewire("../index.js")
 
-
-  /*
-var handlerObj = {
-  APP_ID: 'app_id',
-  registerHandlers: function(h) {
-    handlers = h;
-  },
-  execute: function() {
-  }
-};
-
-var alexaSDKMock = {
-  handler: function (event, context, callback) {
-    return handlerObj
-  }
-};
-*/
-
-// The `handlers` variable in index.js is an object. We will be using one of 
-// its properties, which happens to be a function: `FindVideoByGenreIntent`
+// In index.js, the `handlers` object contains the intent request handlers
+// like `LaunchRequest`, `SessionEndedRequest`, etc.
 var handlers = myModule.__get__("handlers");
 
-// The functions in `handlers` call `this.emit`, `this.reqeuest`, etc.
+// The handler functions in `handlers` call `this.emit`, `this.event`, etc.
 // This will mock up everything in `this`
 
 // Now you can call `this.event...`
@@ -48,27 +32,32 @@ handlers.emit = function(a,b,c,d) {
   calledEmitWithArgs[3] = d;
 }
 
-/*
-console.log('\nhandlers:')
-console.log(handlers);
-*/
 
-// Set up your test with the desired conditions
-handlers.event.request.dialogState = 'IN_PROGRESS';
-handlers.event.request.intent.slots.genre.value = null;
-
-// Then make assertions on calledEmitWithArgs
+// In your tests, your inputs will be properties in handlers.event
+// like `handlers.event.request.dialogState`
+// and your output will be the args passed to `this.emit`
+// like `this.emit(':delegate')`
+// Make assertions on `calledEmitWithArgs`
 describe('FindVideoByGenreIntent', () => {
   it('calls this.emit with 4 specific strings', () => {
+    // Set up your test with the desired conditions
+    handlers.event.request.dialogState = 'IN_PROGRESS';
+    handlers.event.request.intent.slots.genre.value = null;
+
+    // Define your expected args to `this.emit`
     const first = ':elicitSlot';
     const second = 'genre';
-    const third = 'What genre would you like. You can say drama, horror, comedy, action, ';
-    const fourth = 'Please tell me the genre for the video you would like. You can say action, comedy, horror, drama';
+    const third = 'What genre would you like. You can say';
+    const fourth = 'Please tell me the genre for the video you would like. You can say';
+
+    // Call the handler function
     handlers.FindVideoByGenreIntent();
-    assert.equal(calledEmitWithArgs[0], first, 'Expected first arg to be :elicitSlot');
-    assert.equal(calledEmitWithArgs[1], second, 'Expected second arg to be genre');
-    assert.include(calledEmitWithArgs[2], 'What genre', 'Expected third arg to include What genre');
-    assert.equal(calledEmitWithArgs[2], third, 'Expected third arg to be What genre would you...');
+
+    // Make assertions
+    assert.equal(calledEmitWithArgs[0], first, `Expected first arg to be ${first}`);
+    assert.equal(calledEmitWithArgs[1], second, `Expected second arg to be ${second}`);
+    assert.include(calledEmitWithArgs[2], third, `Expected third arg to include: ${third}`);
+    assert.include(calledEmitWithArgs[3], fourth, `Expected fourth arg to include: ${fourth}`);
   });
 });
 
