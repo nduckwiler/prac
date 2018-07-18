c443f8bec6b29559ba79693e2b7d26a4f15d46a0
Nick Duckwiler <nduckwiler@gmail.com>
2018-07-18 08:37:11 -0400

    Implement Circle class

--- p5-prac/fade-out/sketch.js
+++ p5-prac/fade-out/sketch.js
@@ -1,24 +1,34 @@
 var r = 20;
 var circles = [];
 
+class Circle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.r = 20;
+    this.opacity = 255;
+    this.decayRate = 2;
+  }
+
+  update() {
+    this.opacity -= this.decayRate;
+  }
+}
+
+
 function setup() {
   createCanvas(600,400);
 }
 
 function mousePressed() {
-  circles.push({
-    x: mouseX,
-    y: mouseY,
-    w: r,
-    lifespan: 255
-  });
+  circles.push(new Circle(mouseX, mouseY));
 }
 
 function draw() {
   background(0);
   circles.forEach((circle) => {
-    fill(255, 255, 255, circle.lifespan);
-    ellipse(circle.x, circle.y, circle.w);
-    circle.lifespan--;
+    fill(255, 255, 255, circle.opacity);
+    ellipse(circle.x, circle.y, circle.r);
+    circle.update();
   });
 }
