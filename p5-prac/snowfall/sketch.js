d420931a30b7c95524083eba07c2143ef3cb1a50
Nick Duckwiler <nduckwiler@gmail.com>
2018-07-24 08:26:50 -0400

    Make random snowflakes with gravity

--- p5-prac/snowfall/sketch.js
+++ p5-prac/snowfall/sketch.js
@@ -1,14 +1,21 @@
 var snow = [];
+var gravity;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  snow.push(new Snowflake());
+  gravity = createVector(0, 0.1);
 }
 
 function draw() {
   background(0);
+  snow.push(new Snowflake());
   snow.forEach(flake => {
+    flake.applyForce(gravity);
     flake.render();
-  });
+    flake.update(); 
+  }); 
 
+  snow.filter(flake => {
+    return flake.pos.y > width + 20;
+  });
 }
--- p5-prac/snowfall/snowflake.js
+++ p5-prac/snowfall/snowflake.js
@@ -1,13 +1,27 @@
 class Snowflake {
   constructor () {
-    this.pos = createVector(300,200);
-    this.vel = createVector();
+    this.r = random(2, 6);
+    const x = random(width);
+    const y = random(-100, 10);
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 5);
     this.acc = createVector();
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+
+    this.acc.mult(0);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
   }
 
   render() {
     stroke(255);
-    strokeWeight(4);
+    strokeWeight(this.r);
     point(this.pos.x, this.pos.y);
   }
 }
