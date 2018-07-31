9ff713fee19c81f199a3828c2d9e6768ca9d385d
Nick Duckwiler <nduckwiler@gmail.com>
2018-07-31 08:42:22 -0400

    Switch to table format and add setup/teardown

--- luaunit-prac/my_test_suite.lua
+++ luaunit-prac/my_test_suite.lua
@@ -1,21 +1,33 @@
 local lu = require('luaunit')
 
-function testSubOneArg()
-  local str = 'hello'
-  local substr = 'ello'
-  lu.assertEquals(str:sub(2), substr)
-end
+TestSub = {}
+  function TestSub: setUp()
+    self.fname = 'trash.txt'
+    os.remove(self.fname)
+  end
 
-function testSubNegativeArg()
-  local str = 'hello'
-  local substr = 'lo'
-  lu.assertEquals(str:sub(-2), substr)
-end
+  function TestSub: testSubOneArg()
+    local str = 'hello'
+    local substr = 'ello'
+    lu.assertEquals(str:sub(2), substr)
+  end
 
-function testSubError()
-  local str = 'hello'
-  local msg = 'bad argument'
-  lu.assertErrorMsgContains(msg, string.sub)
-end
+  function TestSub: testSubNegativeArg()
+    local str = 'hello'
+    local substr = 'lo'
+    lu.assertEquals(str:sub(-2), substr)
+  end
+
+  function TestSub: testSubError()
+    local str = 'hello'
+    local msg = 'bad argument'
+    lu.assertErrorMsgContains(msg, string.sub)
+  end
+
+  function TestSub: tearDown()
+    os.remove(self.fname)
+  end
+
+-- end of table TestSub
 
 os.exit( lu.LuaUnit.run() )
