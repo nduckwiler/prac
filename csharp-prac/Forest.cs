2a9238d41e407a4f0d6a2c1eb96125a6e0c7d037
Nick Duckwiler <nduckwiler@gmail.com>
2019-05-15 16:32:14 -0400

    Init Forest class
    Next few steps:
    1. Set Age to 0 by default
    2. Define public methods
    3. Define private method
    4. Define static field
    5. Define static method
    ....

---
+++ csharp-prac/Forest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace csharp_prac
+{
+    public class Forest
+    {
+        private int _age;
+        private string _name;
+        private string _biome;
+        private double _trees;
+
+        // Constructor
+        public Forest(string name)
+        {
+            _name = name;
+        }
+
+        // Second overload for constructor
+        public Forest(string name, string biome)
+        {
+            _name = name;
+            _biome = biome;
+        }
+
+        // expression-bodied get and set methods for name property
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        // traditional get and set methods for biome property
+        public string Biome
+        {
+            get { return _biome; }
+            set 
+            { 
+              if (value == "tropical" || value == "temperate")
+              {
+                  _biome = value;
+              } 
+              else
+              {
+                  _biome = "Unknown";
+              }
+            }
+        }
+
+        // shorthand get and set methods for age property
+        public int Age
+        { get; set; }
+    }
+}
--- csharp-prac/Program.cs
+++ csharp-prac/Program.cs
@@ -6,7 +6,17 @@ namespace csharp_prac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Forest f = new Forest("Amazon");
+            Console.WriteLine("First forest-");
+            Console.WriteLine($"Name: {f.Name}");
+            Console.WriteLine($"Age: {f.Age}");
+
+            Forest g = new Forest("Congo", "tropical");
+            Console.WriteLine("Second forest-");
+            Console.WriteLine($"Name: {g.Name}");
+            Console.WriteLine($"Biome: {g.Biome}");
+            Console.WriteLine($"Age: {g.Age}");
+
         }
     }
 }
