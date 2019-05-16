37618df1e4695350246a6ad32023369200fce3bf
Nick Duckwiler <nduckwiler@gmail.com>
2019-05-16 14:33:41 -0400

    Add static TreeFacts and automatic Name props

--- csharp-prac/Forest.cs
+++ csharp-prac/Forest.cs
@@ -3,32 +3,40 @@ namespace csharp_prac
 {
     public class Forest
     {
-        private int _age;
-        private string _name;
+        private int _age = 0;
         private string _biome;
         private double _trees;
+        static string _treeFacts;
 
-        // Constructor
-        public Forest(string name)
+        // Static constructor
+        // Run at most once before any instances are initialized
+        static Forest()
         {
-            _name = name;
+          _treeFacts = "Forests provide a diversity of ecosystem services including:\r\n" +
+"converting carbon dioxide into oxygen and biomass.\r\n" +
+"acting as a carbon sink.\r\n" +
+"aiding in regulating climate.\r\n" +
+"purifying water.\r\n" +
+"mitigating natural hazards such as floods.\r\n" +
+"serving as a genetic reserve.\r\n" +
+"serving as a source of lumber and as recreational are\r\n";
         }
 
-        // Second overload for constructor
+
+        // First constructor
+        public Forest(string name)
+        {
+            Name = name;
+        }
+
+        // Second constructor
         public Forest(string name, string biome)
         {
-            _name = name;
+            Name = name;
             _biome = biome;
         }
 
-        // expression-bodied get and set methods for name property
-        public string Name
-        {
-            get => _name;
-            set => _name = value;
-        }
-
-        // traditional get and set methods for biome property
+        // Biome property: traditional get and set methods
         public string Biome
         {
             get { return _biome; }
@@ -45,8 +53,24 @@ namespace csharp_prac
             }
         }
 
-        // shorthand get and set methods for age property
+        // Age property: only get method
         public int Age
+        {
+            get { return _age; }
+        }
+
+        // Name property: automatic, so a hidden Name field is backing this property
+        public string Name
         { get; set; }
+
+        // TreeFacts property: static get method for a static property
+        public static string TreeFacts
+        {
+            get { return _treeFacts; }
+        }
+
+        // expression-bodied properties are not included here
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/properties#expression-body-definitions
+
     }
 }
--- csharp-prac/Program.cs
+++ csharp-prac/Program.cs
@@ -6,16 +6,22 @@ namespace csharp_prac
     {
         static void Main(string[] args)
         {
+            // Use first constructor
             Forest f = new Forest("Amazon");
             Console.WriteLine("First forest-");
             Console.WriteLine($"Name: {f.Name}");
+            Console.WriteLine($"Biome: {f.Biome}");
             Console.WriteLine($"Age: {f.Age}");
 
+            // Use second constructor
             Forest g = new Forest("Congo", "tropical");
             Console.WriteLine("Second forest-");
             Console.WriteLine($"Name: {g.Name}");
             Console.WriteLine($"Biome: {g.Biome}");
             Console.WriteLine($"Age: {g.Age}");
+
+            // Use static property
+            Console.WriteLine(Forest.TreeFacts);
 
         }
     }
