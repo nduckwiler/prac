293e3402df2f67df5bf5b356a5355b624fc39b22
Nick Duckwiler <nduckwiler@gmail.com>
2019-07-25 13:06:37 -0400

    Fix: Implement interface
    Remember that the implemented interface members MUST be public

--- csharp-prac/InterfacesInheritance/Sedan.cs
+++ csharp-prac/InterfacesInheritance/Sedan.cs
@@ -4,6 +4,14 @@ namespace InterfacesInheritance
 {
     class Sedan : IAutomobile
     {
+        public string LicensePlate
+        { get; }
+
+        public double Speed
+        { get; }
+
+        public int Wheels
+        { get; }
 
     }
 }
