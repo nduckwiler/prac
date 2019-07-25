1300a22ab7bdd4ecfacd72b03e30cce198244f34
Nick Duckwiler <nduckwiler@gmail.com>
2019-07-25 13:02:23 -0400

    Reset for interface-first attempt

--- csharp-prac/InterfacesInheritance/Program.cs
+++ csharp-prac/InterfacesInheritance/Program.cs
@@ -6,20 +6,6 @@ namespace InterfacesInheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Constructing Vehicle type");
-            Vehicle v = new Vehicle(60);
-            Console.WriteLine($"Vehicle' speed: {v.Speed}");
-            v.SpeedUp();
-            v.SpeedUp();
-            Console.WriteLine($"Vehicle's new speed: {v.Speed}");
-
-            Console.WriteLine("Constructing Sedan type");
-            Sedan s = new Sedan(50);
-            Console.WriteLine($"Sedan's speed: {s.Speed}");
-            s.SpeedUp();
-            s.SpeedUp();
-            Console.WriteLine($"Sedan's new speed: {s.Speed}");
-
 
         }
     }
--- csharp-prac/InterfacesInheritance/Sedan.cs
+++ csharp-prac/InterfacesInheritance/Sedan.cs
@@ -2,28 +2,8 @@ using System;
 
 namespace InterfacesInheritance
 {
-    class Sedan : Vehicle
+    class Sedan
     {
-        /*
-        If you delete this constructor, you'll find an error:
-        There is no argument given that corresponds to the required formal parameter 'speed' of 'Vehicle.Vehicle(double)' (CS7036) [InterfacesInheritance]
-        
-        In other words, the Sedan type constructor MUST call a Vehicle constructor.
-        Normally, if we didn't explicitly call the Vehicle constructor,
-        the compiler would implicitly call Vehicle's default parameterless constructor.
-        But that doesn't exist because we defined a one-parameter constructor.
-         */ 
-
-        public Sedan(double speed) : base(speed)
-        {
-
-        }
-
-        public override void SpeedUp()
-        {
-            this.speed += 5;
-
-        }
 
     }
 }
