667541d2a0fdcb7cd4be0577887223ffa2f1e8f8
Nick Duckwiler <nduckwiler@gmail.com>
2019-07-25 13:54:20 -0400

    Issue: Add different SpeedUp/SlowDown to Bicycle
    We'd like to change the SpeedUp/SlowDown methods for Bicycle. We are
    essentially "hiding" the Vehicle class' SpeedUp/SlowDown methods.
    
    The errors:
    
    Bicycle.cs(13,17): warning CS0114: 'Bicycle.SpeedUp()' hides inherited
    member 'Vehicle.SpeedUp()'. To make the current member override that
    implementation, add the override keyword. Otherwise add the new keyword.
    [/Users/nicholasduckwiler/Repos/prac/csharp-prac/InterfacesInheritance/InterfacesInheritance.csproj]
    Bicycle.cs(24,17): warning CS0114: 'Bicycle.SlowDown()' hides inherited
    member 'Vehicle.SlowDown()'. To make the current member override that
    implementation, add the override keyword. Otherwise add the new keyword.
    [/Users/nicholasduckwiler/Repos/prac/csharp-prac/InterfacesInheritance/InterfacesInheritance.csproj]

--- csharp-prac/InterfacesInheritance/Bicycle.cs
+++ csharp-prac/InterfacesInheritance/Bicycle.cs
@@ -10,5 +10,26 @@ namespace InterfacesInheritance
       Wheels = 2;
     }
 
+    public void SpeedUp()
+    {
+      Speed += 5;
+      
+      if (Speed > 15)
+      {
+        Speed = 15;
+      }
+
+    }
+
+    public void SlowDown()
+    {
+      Speed -= 5;
+
+      if (Speed < 0)
+      {
+        Speed = 0;
+      }
+    }
+
   }
 }
